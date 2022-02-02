c5d38560c71e5ad6ae5745f3bc15ca29d31c6a0a
JonasN05 <jonas.nigg@student.htldornbirn.at>
2022-02-02 16:07:00 +0100

    Test

---
+++ Test/script.js
@@ -0,0 +1,40 @@
+document.getElementById("button").addEventListener("click", function () {
+    giveResult();
+});
+
+function giveResult() {
+    let number = document.getElementById("textfield").value;
+
+    let rounded = Math.round(number);
+    console.log(rounded);
+
+    if (rounded > 0) {
+        let output = "";
+        for (index = 0; index <= rounded; index++) {
+            
+            if (index < rounded){
+                output += index + ", ";
+            } else{
+                output += index;
+            }
+
+            document.getElementById("result").innerHTML = output;
+        }
+
+    } else if (rounded < 0) {
+        let output = "";
+        for (index = rounded; index <= 0; index++) {
+
+            if(0 > index){
+                output += index + ", ";
+            } else{
+                output += index;
+            }
+            
+            document.getElementById("result").innerHTML = output;
+        }
+        
+    }else{
+        document.getElementById("result").innerHTML = 0;
+    }
+}
